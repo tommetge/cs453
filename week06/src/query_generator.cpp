74bebefd2365e4f3c1daf69c3e98e6c0017286c1
tom metge <tommetge@apple.com>
2021-10-20 16:43:38 -0700

    Add base query generator and sample test case

--- week06/src/query_generator.cpp
+++ week06/src/query_generator.cpp
@@ -1,7 +1,14 @@
 #include "query_generator.hpp"
 
+#include <stdio.h>
+
 using namespace std;
 
+static const char* SQLAuthStatement = "SELECT COUNT(*) FROM users WHERE username = '%s' AND password = '%s';";
+
 string cs453::generateQuery(string &username, string &password) {
-    return string();
+    char buff[1024];
+    snprintf(buff, 1024, SQLAuthStatement, username.c_str(), password.c_str());
+
+    return string(buff);
 }
--- week06/test/test-valid.cpp
+++ week06/test/test-valid.cpp
@@ -3,8 +3,18 @@
 #include "query_generator.hpp"
 
 using namespace cs453;
+using namespace std;
+
+bool testGenerateQuery(const char *username, const char *password, const char *expected) {
+    string user = string(username);
+    string pass = string(password);
+    string query = generateQuery(user, pass);
+    string expect = string(expected);
+    return query == expect;
+}
 
 TEST_CASE( "Valid test cases", "[valid]" )
 {
-    REQUIRE( true == true );
+    REQUIRE( testGenerateQuery("tom", "pass", "SELECT COUNT(*) FROM users WHERE username = 'tom' AND password = 'pass';"));
+    REQUIRE( testGenerateQuery("tom2@example1.com", "pass1234!", "SELECT COUNT(*) FROM users WHERE username = 'tom2@example1.com' AND password = 'pass1234!';"));
 }
