3e14c46def598d1b617820dcc4aba89605b3a5c2
tom metge <tommetge@apple.com>
2021-11-03 12:20:34 -0700

    Solutions for week 8

--- week08/src/main.cpp
+++ week08/src/main.cpp
@@ -9,8 +9,40 @@ using namespace std;
  * 2. The array index variable must be reachable through external input.
  * 3. There must not be bounds checking on the array index variable.
  ****************************************/
-void arrayVulnerability(/* feel free to add parameters */)
+int arrayVulnerability(int index, int score)
 {
+   // Array of assignment scores (each score is out of 100)
+   int scores[5] = { 100, 100, 100, 100, 0 };
+
+   // Weighting for each assignment
+   // Note: this is the protected asset
+   int weights[5] = { 10, 20, 30, 40, 50 };
+   // cout << "Weights address: " << hex << &weights[0] << endl;
+   // cout << "Scores address: " << hex << &scores[0] << endl;
+   // cout << "Targeted address: " << hex << &scores[index] << endl;
+
+   // Sanitize our input: no scores > 100 or < 0!
+   if (score > 100) {
+      score = 100;
+   }
+   if (score < 0) {
+      score = 0;
+   }
+
+   scores[index] = score;
+
+   // Do some quick math to get an overall score by multiplying
+   // each score by its weight and averaging them.
+   int weightedScores = 0;
+   int totalWeights = 0;
+   for (int i=0; i<5; i++) {
+      weightedScores += scores[i] * weights[i];
+      totalWeights += weights[i];
+      // cout << "\t" << dec << scores[i] << " | " << weights[i] << endl;
+   }
+
+   return weightedScores / totalWeights;
+   // cout << "Weighted score: " << dec << weightedScores / totalWeights << endl;
 }
 
 /**************************************
@@ -20,7 +52,15 @@ void arrayVulnerability(/* feel free to add parameters */)
  *************************************/
 void arrayWorking()
 {
-   arrayVulnerability(/* parameters with non-malicious data */);
+   // Give ourselves a full marks on the last (ungraded) assignment
+   int totalScore = arrayVulnerability(4, 100);
+   // Our weighted average should be 100
+   cout << "Overall score (working, expecting 100):\t\t" << totalScore << endl;
+
+   // Update our last assignment grade to 80
+   totalScore = arrayVulnerability(4, 80);
+   // This will lower our overall score to 93
+   cout << "Overall score (working, expecting 93):\t\t" << totalScore << endl;
 }
 
 /**************************************
@@ -33,15 +73,43 @@ void arrayWorking()
  *************************************/
 void arrayExploit()
 {
-   arrayVulnerability(/* parameters with malicious data */);
+   int totalScore = arrayVulnerability(4, 80);
+   cout << "Overall score (pre-exploit, expecting 93):\t" << totalScore << endl;
+
+   // At this point, our total score will be 93. Let's bump it by
+   // exploiting the array index to change the weight for the final
+   // assignment. We use a negative index to write our 'score' into
+   // the protected array of weights, changing our grade to 99.
+   totalScore = arrayVulnerability(-4, 1);
+   cout << "Overall score (post-exploit, expecting 93):\t" << totalScore << endl;
+}
+
+void arcSafeFunction() {
+   cout << "Safe!" << endl;
+}
+
+void arcUnsafeFunction() {
+   cout << "UNSAFE!!" << endl;
 }
 
 /*************************************
  * ARC VULNERABILTY
  * ...
  ****************************************/
-void arcVulnerability(/* feel free to add parameters */)
+void arcVulnerability(const char *input)
 {
+   // Setup our function pointer to arcSafeFunction()
+   void (* pointerToSafe)() = arcSafeFunction;
+
+   // Make a small buffer
+   char buffer[4];
+
+   // Note: using memcpy + strlen here is the vulnerability - it will
+   // overrun the buffer.
+   memcpy(buffer, input, strlen(input));
+
+   // Execute our (hopefully) safe function
+   pointerToSafe();
 }
 
 /**************************************
@@ -51,7 +119,7 @@ void arcVulnerability(/* feel free to add parameters */)
  *************************************/
 void arcWorking()
 {
-   arcVulnerability(/* parameters with non-malicious data */);
+   arcVulnerability("Hi\0\0");
 }
 
 /**************************************
@@ -60,15 +128,68 @@ void arcWorking()
  *************************************/
 void arcExploit()
 {
-   arcVulnerability(/* parameters with malicious data */);
+   // Initialize our input with zeros, make it big enough to hold
+   // the buffer size + function pointer.
+   char maliciousInput[4 + sizeof(long *)] = { 0 };
+
+   // Set our input to something strcpy won't mind (no null character)
+   maliciousInput[0] = 0x42;
+   maliciousInput[1] = 0x61;
+   maliciousInput[2] = 0x64;
+   maliciousInput[3] = 0x21;
+
+   // Set the remainder of our input to the address of arcUnsafeFunction()
+   void (* pointerToUnsafe)() = arcUnsafeFunction;
+   memcpy(maliciousInput + 4, (long *)&pointerToUnsafe, sizeof(long *));
+
+   arcVulnerability(maliciousInput);
+}
+
+// Simple classes to demonstrate vptr / vtable manipulation
+class MyClass {
+public:
+   virtual void sayHi() {
+      cout << "Hello!" << endl;
+   }
+private:
+   char buffer[4] = "Hi\0";
+};
+
+class MyUnsafeClass: MyClass {
+public:
+   virtual void sayHi() {
+      cout << "UNSAFE!!" << endl;
+   }
+};
+
+void vtableSayHi(MyClass *myClass) {
+   myClass->sayHi();
 }
 
 /*************************************
  * VTABLE VULNERABILTY
  * ...
  ****************************************/
-void vtableVulnerability(/* feel free to add parameters */)
+void vtableVulnerability(const char *input)
 {
+   // Overall note: clang on macOS (x86_64) puts the vptr before any
+   // class members (like buffer), so there's no way to reproduce the
+   // vtable spraying attack as canonized in the reading - at least
+   // on this class of machines using this compiler. It is, however,
+   // possible to use a stack smashing attack to accomplish the same:
+   // change the vptr of a class. This is what is implemented here.
+
+   MyClass myClass = MyClass();
+   char buffer[2];
+
+   // Note: using memcpy + strlen here is the vulnerability - it will
+   // overrun the buffer. Given the positions in the stack, it allows
+   // the caller to potentially overwrite myClass' vptr.
+   memcpy(buffer, input, strlen(input));
+
+   // Note: this indirection is necessary because otherwise the vptr
+   // (that may be overwritten) is cached.
+   vtableSayHi(&myClass);
 }
 
 /**************************************
@@ -78,7 +199,7 @@ void vtableVulnerability(/* feel free to add parameters */)
  *************************************/
 void vtableWorking()
 {
-   vtableVulnerability(/* parameters with non-malicious data */);
+   vtableVulnerability("Hi");
 }
 
 /**************************************
@@ -87,15 +208,34 @@ void vtableWorking()
  *************************************/
 void vtableExploit()
 {
-   vtableVulnerability(/* parameters with malicious data */);
+   // Initialize our input with zeros, make it big enough to hold
+   // the buffer size + function pointer.
+   char maliciousInput[2 + sizeof(long *) + sizeof(long *)] = { 0 };
+
+   // Set our input to something strcpy won't mind (no null character)
+   maliciousInput[0] = 0x48;
+   maliciousInput[1] = 0x69;
+
+   // Set the remainder of our input to the address of MyUnsafeClass' VTable
+   MyUnsafeClass myUnsafeClass = MyUnsafeClass();
+   long **myUnsafeClassVTable = (long **)((long *)&myUnsafeClass)[0];
+   memcpy(maliciousInput + 2, (long *)&myUnsafeClassVTable, sizeof(long *));
+   memcpy(maliciousInput + 2 + sizeof(long *), (long *)&myUnsafeClassVTable, sizeof(long *));
+
+   vtableVulnerability(maliciousInput);
 }
 
 /*************************************
  * STACK VULNERABILTY
  * ...
  ****************************************/
-void stackVulnerability(/* feel free to add parameters */)
+void stackVulnerability(const string &str)
 {
+   char buffer[8] = { 0 };
+
+   // Note: using memcpy + strlen here is the vulnerability - it will
+   // overrun the buffer.
+   memcpy(buffer, str.c_str(), str.length());
 }
 
 /**************************************
@@ -105,8 +245,11 @@ void stackVulnerability(/* feel free to add parameters */)
  *************************************/
 void stackWorking()
 {
-   stackVulnerability(/* parameters with non-malicious data */);
+   char msg[8] = "Hi!\0\0\0\0";
+   stackVulnerability(msg);
 }
+
+static bool stackIsHealthy = false;
 
 /**************************************
  * STACK EXPLOIT
@@ -114,15 +257,52 @@ void stackWorking()
  *************************************/
 void stackExploit()
 {
-   stackVulnerability(/* parameters with malicious data */);
+   long anchor = 0;
+   char exploit[32] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+   printf("Stack is");
+
+   // Fetch our canary
+   long canary = *((long *)(&anchor + 6));
+
+   // Fetch our frame pointer
+   long framePtr = *((long *)(&anchor - 11));
+
+   // Fetch the return address
+   long returnAddr = *((long *)(&anchor - 10)); // 2a35
+   returnAddr += 145; // 2ac6
+   // Note: The following will only work when run under
+   // a debugger
+   // long returnAddr = 0x100002acb;
+
+   memcpy(exploit + 8, &canary, sizeof(long *));
+   memcpy(exploit + 16, &framePtr, sizeof(long *));
+   memcpy(exploit + 24, &returnAddr, sizeof(long *));
+
+   string *exploitStr = new string(exploit, 32);
+   stackVulnerability(*exploitStr);
+   printf("NOT ");
+   printf(" smashed!\n");
 }
 
 /*************************************
  * HEAP VULNERABILTY
  * ...
  ****************************************/
-void heapVulnerability(/* feel free to add parameters */)
+void heapVulnerability(const string &input)
 {
+   char *buff1 = (char *)malloc(4);
+   char *buff2 = (char *)malloc(4);
+
+   assert(buff1 < buff2);
+   memcpy(buff1, input.c_str(), input.length());
+
+   free(buff2);
+   free(buff1);
+
+   char *buff3 = (char *)malloc(12);
+   strncpy(buff3, "Heap safe!\n", 11);
+   printf("%s\n", buff3);
 }
 
 /**************************************
@@ -132,7 +312,7 @@ void heapVulnerability(/* feel free to add parameters */)
  *************************************/
 void heapWorking()
 {
-   heapVulnerability(/* parameters with non-malicious data */);
+   heapVulnerability("Working");
 }
 
 /**************************************
@@ -141,15 +321,29 @@ void heapWorking()
  *************************************/
 void heapExploit()
 {
-   heapVulnerability(/* parameters with malicious data */);
+   char exploit[32] = "aaaaaaaaaaaNOT!";
+   string *exploitStr = new string(exploit, 32);
+   heapVulnerability(*exploitStr);
 }
 
 /*************************************
  * INTEGER OVERFLOW VULNERABILTY
  * ...
  ****************************************/
-void intOverflowVulnerability(/* feel free to add parameters */)
+void intOverflowVulnerability(int octalPerms)
 {
+   // This is a subtle one. We're incorrectly comparing an int
+   // with the least significant 3 binary digits. We're expecting
+   // an POSIX file permissions octal-style pattern (i.e. 0755)
+   // but going about the check all wrong.
+   // For reference: POSIX permissions should be checked
+   // bit-by-bit in binary.
+   if (octalPerms >= 07) {
+      cout << "Access granted" << endl;
+      return;
+   }
+
+   cout << "Access denied!" << endl;
 }
 
 /**************************************
@@ -159,7 +353,7 @@ void intOverflowVulnerability(/* feel free to add parameters */)
  *************************************/
 void intOverflowWorking()
 {
-   intOverflowVulnerability(/* parameters with non-malicious data */);
+   intOverflowVulnerability(01);
 }
 
 /**************************************
@@ -168,15 +362,18 @@ void intOverflowWorking()
  *************************************/
 void intOverflowExploit()
 {
-   intOverflowVulnerability(/* parameters with malicious data */);
+   intOverflowVulnerability(100);
 }
 
 /*************************************
  * ANSII-UNICODE VULNERABILTY
  * ...
  ****************************************/
-void ansiiToUnicodeVulnerability(/* feel free to add parameters */)
+void ansiiToUnicodeVulnerability(short *input)
 {
+   short buff[32];
+   memcpy(buff, input, sizeof(buff) * sizeof(short));
+   cout << buff << endl;
 }
 
 /**************************************
@@ -186,7 +383,8 @@ void ansiiToUnicodeVulnerability(/* feel free to add parameters */)
  *************************************/
 void ansiiToUnicodeWorking()
 {
-   ansiiToUnicodeVulnerability(/* parameters with non-malicious data */);
+   short msg[5] = { 0x53, 0x61, 0x66, 0x65, 0x21 };
+   ansiiToUnicodeVulnerability(msg);
 }
 
 /**************************************
@@ -195,7 +393,10 @@ void ansiiToUnicodeWorking()
  *************************************/
 void ansiiToUnicodeExploit()
 {
-   ansiiToUnicodeVulnerability(/* parameters with malicious data */);
+   // Note: Running this with ASAN enabled will show the buffer
+   // overflow. Running it as-is will abort.
+   short msg[64] = {};
+   ansiiToUnicodeVulnerability(msg);
 }
 
 static const char *SectionBoundary = "********************\n";
@@ -208,49 +409,42 @@ int main()
    cout << "Array Index" << endl;
    cout << SectionBoundary << endl;
 
-   arrayVulnerability();
    arrayWorking();
    arrayExploit();
 
-   cout << "ARC" << endl;
+   cout << endl << "ARC" << endl;
    cout << SectionBoundary << endl;
 
-   arcVulnerability();
    arcWorking();
    arcExploit();
 
-   cout << "VTABLE" << endl;
+   cout << endl << "VTABLE" << endl;
    cout << SectionBoundary << endl;
 
-   vtableVulnerability();
    vtableWorking();
    vtableExploit();
 
-   cout << "Stack Smashing" << endl;
+   cout << endl << "Stack Smashing" << endl;
    cout << SectionBoundary << endl;
 
-   stackVulnerability();
    stackWorking();
    stackExploit();
 
-   cout << "Heap Spraying" << endl;
+   cout << endl << "Heap Spraying" << endl;
    cout << SectionBoundary << endl;
 
-   heapVulnerability();
    heapWorking();
    heapExploit();
 
-   cout << "Integer Overflow" << endl;
+   cout << endl << "Integer Overflow" << endl;
    cout << SectionBoundary << endl;
 
-   intOverflowVulnerability();
    intOverflowWorking();
    intOverflowExploit();
 
-   cout << "VTABLE" << endl;
+   cout << endl << "ASCII to Unicode" << endl;
    cout << SectionBoundary << endl;
 
-   ansiiToUnicodeVulnerability();
    ansiiToUnicodeWorking();
    ansiiToUnicodeExploit();
 
