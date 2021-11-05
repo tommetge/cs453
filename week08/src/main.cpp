772f6543cd1faaa04d1b2c4f5bb9f5eea8c604e2
Porter McGary <mcg15003@byui.edu>
2021-11-05 16:27:36 -0600

    Ansii and arc

--- week08/src/main.cpp
+++ week08/src/main.cpp
@@ -3,45 +3,36 @@
 
 using namespace std;
 
-/*************************************
- * ARRAY VULNERABILTY
- * 1. There must be an array and an array index variable
- * 2. The array index variable must be reachable through external input.
- * 3. There must not be bounds checking on the array index variable.
- ****************************************/
-void arrayVulnerability(/* feel free to add parameters */)
-{
+void safe() {
+   printf("Safe function\n");
 }
 
-/**************************************
- * ARRAY WORKING
- * Call arrayVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void arrayWorking()
-{
-   arrayVulnerability(/* parameters with non-malicious data */);
-}
-
-/**************************************
- * ARRAY EXPLOIT
- * 1. The attacker provides an array index value outside the expected range
- * 2. The attacker must be able to provide input or redirect
- *    existing input into the array at the index he provided
- * 3. The injected value must alter program state in a way
- *    that is desirable to the attacker
- *************************************/
-void arrayExploit()
-{
-   arrayVulnerability(/* parameters with malicious data */);
+void unsafe() {
+   printf("UNSAFE FUNCTION!!!!\n");
 }
 
 /*************************************
  * ARC VULNERABILTY
- * ...
+ * 1. There must be a function pointer used in the code.
+ * 2. Through some vulnerability, there must be a way for user input 
+ *    to overwrite the function pointer. This typically happens through 
+ *    a stack buffer vulnerability.
+ * 3. After the memory is overwritten, the function pointer must 
+ *    be dereferenced.
  ****************************************/
-void arcVulnerability(/* feel free to add parameters */)
+// accept a buffer
+void arcVulnerability(const char * input)
 {
+   // set the function ptr to the safe function
+   void (* functionPtr)() = safe;
+   // set the buffer
+   char buffer[4] = {0};
+
+   // create vulnerability from the buffer by not checking the length of the input
+   memcpy(buffer, input, strlen(input));
+  
+   // call the now unsafe pointer
+   functionPtr();
 }
 
 /**************************************
@@ -51,7 +42,7 @@ void arcVulnerability(/* feel free to add parameters */)
  *************************************/
 void arcWorking()
 {
-   arcVulnerability(/* parameters with non-malicious data */);
+   arcVulnerability("Home");
 }
 
 /**************************************
@@ -60,123 +51,36 @@ void arcWorking()
  *************************************/
 void arcExploit()
 {
-   arcVulnerability(/* parameters with malicious data */);
-}
 
-/*************************************
- * VTABLE VULNERABILTY
- * ...
- ****************************************/
-void vtableVulnerability(/* feel free to add parameters */)
-{
-}
+   // create buffer
+   char bad[4 + sizeof(long *)] = {0};
 
-/**************************************
- * VTABLE WORKING
- * Call vtableVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void vtableWorking()
-{
-   vtableVulnerability(/* parameters with non-malicious data */);
-}
+   // create function pointer that points to safe
+   bad[0] = 0xaa;
+   bad[1] = 0xaa;
+   bad[2] = 0xaa;
+   bad[3] = 0xaa;
 
-/**************************************
- * VTABLE EXPLOIT
- * ...
- *************************************/
-void vtableExploit()
-{
-   vtableVulnerability(/* parameters with malicious data */);
-}
+   // overflow the buffer to point to the unsafe function
+   void (* unsafePtr)() = unsafe;
+   memcpy(bad + 4, (long *)&unsafePtr, sizeof(long *));
 
-/*************************************
- * STACK VULNERABILTY
- * ...
- ****************************************/
-void stackVulnerability(/* feel free to add parameters */)
-{
-}
-
-/**************************************
- * STACK WORKING
- * Call stackVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void stackWorking()
-{
-   stackVulnerability(/* parameters with non-malicious data */);
-}
-
-/**************************************
- * STACK EXPLOIT
- * ...
- *************************************/
-void stackExploit()
-{
-   stackVulnerability(/* parameters with malicious data */);
-}
-
-/*************************************
- * HEAP VULNERABILTY
- * ...
- ****************************************/
-void heapVulnerability(/* feel free to add parameters */)
-{
-}
-
-/**************************************
- * HEAP WORKING
- * Call heapVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void heapWorking()
-{
-   heapVulnerability(/* parameters with non-malicious data */);
-}
-
-/**************************************
- * HEAP EXPLOIT
- * ...
- *************************************/
-void heapExploit()
-{
-   heapVulnerability(/* parameters with malicious data */);
-}
-
-/*************************************
- * INTEGER OVERFLOW VULNERABILTY
- * ...
- ****************************************/
-void intOverflowVulnerability(/* feel free to add parameters */)
-{
-}
-
-/**************************************
- * INTEGER OVERFLOW WORKING
- * Call intOverflowVulnerability() in a way that does
- * not yield unexpected behavior
- *************************************/
-void intOverflowWorking()
-{
-   intOverflowVulnerability(/* parameters with non-malicious data */);
-}
-
-/**************************************
- * INTEGER OVERFLOW EXPLOIT
- * ...
- *************************************/
-void intOverflowExploit()
-{
-   intOverflowVulnerability(/* parameters with malicious data */);
+   arcVulnerability(bad);
 }
 
 /*************************************
  * ANSII-UNICODE VULNERABILTY
- * ...
+ * 1. There must be a buffer where the basetype is greater than one.
+ * 2. Validation of the buffer must check the size of the buffer rather 
+      than the number of elements in the buffer.
  ****************************************/
-void ansiiToUnicodeVulnerability(/* feel free to add parameters */)
+void ansiiToUnicodeVulnerability(long * input)
 {
+   // Buffer that is vulnerable
+   long buffer[32];
+   // Not checking the number of elements and instead checking the size creates the vulnerability
+   memcpy(buffer, input, sizeof(buffer) * sizeof(short));
+   cout << buffer << endl;
 }
 
 /**************************************
@@ -186,7 +90,8 @@ void ansiiToUnicodeVulnerability(/* feel free to add parameters */)
  *************************************/
 void ansiiToUnicodeWorking()
 {
-   ansiiToUnicodeVulnerability(/* parameters with non-malicious data */);
+   long lng[4] = {0xaa, 0xaa, 0xaa, 0xaa};
+   ansiiToUnicodeVulnerability(lng);
 }
 
 /**************************************
@@ -195,62 +100,64 @@ void ansiiToUnicodeWorking()
  *************************************/
 void ansiiToUnicodeExploit()
 {
-   ansiiToUnicodeVulnerability(/* parameters with malicious data */);
+   // NOTE: This will cause the program to abort. To see the actual overflow use ASAN
+   long bad[64] = {};
+   ansiiToUnicodeVulnerability(bad);
 }
 
-static const char *SectionBoundary = "********************\n";
+static const char *SectionBoundary = "********************";
 
 /**********************************************
  * MAIN : The main function.
  **********************************************/
 int main()
 {
-   cout << "Array Index" << endl;
+   // cout << "Array Index" << endl;
+   // cout << SectionBoundary << endl;
+
+   // arrayVulnerability();
+   // arrayWorking();
+   // arrayExploit();
+
+   cout << "\nARC" << endl;
    cout << SectionBoundary << endl;
 
-   arrayVulnerability();
-   arrayWorking();
-   arrayExploit();
-
-   cout << "ARC" << endl;
-   cout << SectionBoundary << endl;
-
-   arcVulnerability();
+   // arcVulnerability();
    arcWorking();
    arcExploit();
 
-   cout << "VTABLE" << endl;
+   // cout << "VTABLE" << endl;
+   // cout << SectionBoundary << endl;
+
+   // vtableVulnerability();
+   // vtableWorking();
+   // vtableExploit();
+
+   // cout << "Stack Smashing" << endl;
+   // cout << SectionBoundary << endl;
+
+   // stackVulnerability();
+   // stackWorking();
+   // stackExploit();
+
+   // cout << "Heap Spraying" << endl;
+   // cout << SectionBoundary << endl;
+
+   // heapVulnerability();
+   // heapWorking();
+   // heapExploit();
+
+   // cout << "Integer Overflow" << endl;
+   // cout << SectionBoundary << endl;
+
+   // intOverflowVulnerability();
+   // intOverflowWorking();
+   // intOverflowExploit();
+
+   cout << "\nANSI-UNICODE" << endl;
    cout << SectionBoundary << endl;
 
-   vtableVulnerability();
-   vtableWorking();
-   vtableExploit();
-
-   cout << "Stack Smashing" << endl;
-   cout << SectionBoundary << endl;
-
-   stackVulnerability();
-   stackWorking();
-   stackExploit();
-
-   cout << "Heap Spraying" << endl;
-   cout << SectionBoundary << endl;
-
-   heapVulnerability();
-   heapWorking();
-   heapExploit();
-
-   cout << "Integer Overflow" << endl;
-   cout << SectionBoundary << endl;
-
-   intOverflowVulnerability();
-   intOverflowWorking();
-   intOverflowExploit();
-
-   cout << "VTABLE" << endl;
-   cout << SectionBoundary << endl;
-
-   ansiiToUnicodeVulnerability();
+   // ansiiToUnicodeVulnerability();
    ansiiToUnicodeWorking();
    ansiiToUnicodeExploit();
 
